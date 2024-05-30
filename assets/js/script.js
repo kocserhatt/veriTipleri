62d6f7a71e5c052a0a84a0700a6fc935ef41fddf
serhat koç <kocserhat671@gmail.com>
2024-05-30 17:45:04 +0300

    veri tipleri ile örnekler

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+// String (Dize) veri tipi örneği
+const stringOrnek = "Merhaba, dünya!";
+console.log("String (Dize) Örneği:", stringOrnek);
+
+// Number (Sayı) veri tipi örneği
+const numberOrnek = 42;
+console.log("Number (Sayı) Örneği:", numberOrnek);
+
+// Boolean (Mantıksal) veri tipi örneği
+const booleanOrnek = true;
+console.log("Boolean (Mantıksal) Örneği:", booleanOrnek);
+
+// Array (Dizi) veri tipi örneği
+const arrayOrnek = [1, 2, 3, 4, 5];
+console.log("Array (Dizi) Örneği:", arrayOrnek);
+
+// Object (Nesne) veri tipi örneği
+const objectOrnek = { ad: "Ahmet", yas: 30, sehir: "İstanbul" };
+console.log("Object (Nesne) Örneği:", objectOrnek);
+
+// Undefined (Tanımsız) veri tipi örneği
+let undefinedOrnek;
+console.log("Undefined (Tanımsız) Örneği:", undefinedOrnek);
+
+// Null (Boş) veri tipi örneği
+const nullOrnek = null;
+console.log("Null (Boş) Örneği:", nullOrnek);
+
+// Symbol (Sembol) veri tipi örneği
+const symbolOrnek = Symbol("açıklama");
+console.log("Symbol (Sembol) Örneği:", symbolOrnek);
